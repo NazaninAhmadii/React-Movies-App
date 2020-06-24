60b90b5e1190726294fae8106fa16337529f1c85
Nazanin <nazaninpolite@gmail.com>
2020-06-24 02:23:21 -0700

    Updates

---
+++ precache-manifest.13660890a8098476cf43c51b6771de1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016ec11e59f16e53e415461492b5a8cf",
+    "url": "/React-Movies-App/index.html"
+  },
+  {
+    "revision": "c590d9608f0a0c6ed3ac",
+    "url": "/React-Movies-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5c1cd0745b5f8087b34e",
+    "url": "/React-Movies-App/static/js/2.fcec4578.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Movies-App/static/js/2.fcec4578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c590d9608f0a0c6ed3ac",
+    "url": "/React-Movies-App/static/js/main.1e0cc666.chunk.js"
+  },
+  {
+    "revision": "a1b3318d1c9989895d7e",
+    "url": "/React-Movies-App/static/js/runtime-main.5be4bb51.js"
+  }
+]);
